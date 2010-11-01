4344cb218b47a9021e503bb3d0df3bfd8772a490
Johan Angelstam <johan@angelstam.se>
2010-11-01 11:30:04 +0100

    Created Main and updated README.

---
+++ src/multimil/Main.java
@@ -0,0 +1,20 @@
+package multimil;
+
+/**
+ * Main class for Multimil.
+ * 
+ * @author angelstam
+ */
+public class Main
+{
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Multimil disassembler for SY6502.");
+		
+	}
+	
+}
